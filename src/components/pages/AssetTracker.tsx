452f5e8747753123ee7f840d09d3eaa9890861bd
Belat <enochbelawu8@gmail.com>
2026-09-22 23:49:07 +0100

    feat: enhance AssetTracker and AssetTrackerTable components with dynamic asset calculations and filtering functionality

--- src/components/pages/AssetTracker.tsx
+++ src/components/pages/AssetTracker.tsx
@@ -1,97 +1,102 @@
 'use client';
 import React from 'react'
-import { IoBagOutline, IoTriangleSharp } from 'react-icons/io5';
-import { ArrowUp, PlusIcon, RefreshCcw, TrendingDown } from 'lucide-react';
+import { IoTriangleSharp } from 'react-icons/io5';
+import { PlusIcon, RefreshCcw, TrendingDown } from 'lucide-react';
 import { useState } from 'react';
-import { CiBag1 } from 'react-icons/ci';
-import { IoTrashBinOutline } from "react-icons/io5";
-import { FaRegTrashAlt } from "react-icons/fa";
 import AddAssetModal from '../../components/ui/AddAssetModal';
 import AssetTrackerTable from '../../components/ui/AssetTrackerTable';
+import { useAssetData } from '@/hooks/useAssetData';
 
 type Props = {}
 
 const AssetTracker = (props: Props) => {
+    const { assets, loading, error } = useAssetData();
+    const liveAssets = assets.filter(asset => asset.status === 'live');
+    const totalValue = liveAssets.reduce((acc, asset) => acc + (asset.purchasePrice * asset.quantity), 0);
+    const totalPnl = liveAssets.reduce((acc, asset) => acc + (asset.purchasePrice * asset.quantity * 0.1), 0);
+    const assetNumber = liveAssets.length;
+    const totalROI = totalValue > 0 ? (totalPnl / totalValue) * 100 : 0;
     const [isAddAssetModalOpen, setIsAddAssetModalOpen] = useState(false);
-    const filters = [
-        { name: 'live assets', value: 'live' },
-        { name: 'closed assets', value: 'closed' },
-        { name: 'deleted assets', value: 'deleted' },
-    ];
     return (
-        <div className='lg:ms-[237px] min-h-screen bg-[var(--bg-canvas)] mb-25 lg:mb-10 p-5 lg:w-[calc(100%-237px)]'>
-            <div className="flex flex-row justify-between items-center">
-                <div className="flex flex-col gap-4 items-start lg:flex-row justify-between lg:items-center w-full">
-                    <div className="flex flex-row justify-between items-center gap-4">
-                        <div className="flex flex-col gap-1">
-                            <h2 className="text-[var(--text-primary)] font-semibold text-[22px] leading-7 tracking-[-0.012em]">Asset Tracker</h2>
-                            <p className="text-[13px] leading-[19px] font-normal text-[var(--text-secondary)]">
-                                Track your crypto portfolio and investment performance
-                            </p>
+        <div className='xl:ms-[237px] transition-all duration-300 min-h-screen bg-[var(--bg-canvas)] p-8'>
+            <div className=" w-full max-w-[1440px] mx-auto">
+                <div className="flex flex-row justify-between items-center">
+                    <div className="flex flex-col gap-4 items-start lg:flex-row justify-between lg:items-center w-full">
+                        <div className="flex flex-row justify-between items-center gap-4">
+                            <div className="flex flex-col gap-1">
+                                <h2 className="text-[var(--text-primary)] font-semibold text-[22px] leading-7 tracking-[-0.012em]">Asset Tracker</h2>
+                                <p className="text-[13px] leading-[19px] font-normal text-[var(--text-secondary)]">
+                                    Track your crypto portfolio and investment performance
+                                </p>
+                            </div>
+                        </div>
+                        <div className="flex items-center justify-center gap-3">
+                            <button className="bg-(--bg-surface) border border-[var(--border-strong)] rounded-[8px] px-2 lg:px-4 py-2 text-[13px] leading-[19px] font-semibold text-[var(--text-primary)] hover:bg-[var(--bg-surface-2)] transition cursor-pointer flex items-center capitalize">
+                                <RefreshCcw className="inline-block me-1 text-black" size={14} />
+                                refresh
+                            </button>
+                            <button className="bg-(--bg-surface) border border-(--border-strong) rounded-[8px] px-2 lg:px-4 py-2 text-[13px] leading-[19px] font-semibold text-(--text-primary) hover:bg-[var(--brand-hover)] transition cursor-pointer flex items-center capitalize" onClick={() => setIsAddAssetModalOpen(true)}>
+                                <PlusIcon className="inline-block me-1" size={14} />
+                                Add Asset
+                            </button>
+                            <button className="bg-(--brand) rounded-[8px] px-2 lg:px-4 py-2 text-[13px] leading-[19px] font-semibold text-white hover:opacity-90 transition cursor-pointer flex items-center capitalize">
+                                <TrendingDown className="inline-block me-1" size={14} />
+                                Add exit
+                            </button>
                         </div>
                     </div>
-                    <div className="flex items-center justify-center gap-3">
-                        <button className="bg-(--bg-surface) border border-[var(--border-strong)] rounded-[8px] px-2 lg:px-4 py-2 text-[13px] leading-[19px] font-semibold text-[var(--text-primary)] hover:bg-[var(--bg-surface-2)] transition cursor-pointer flex items-center capitalize">
-                            <RefreshCcw className="inline-block me-1 text-black" size={14} />
-                            refresh
-                        </button>
-                        <button className="bg-(--bg-surface) border border-(--border-strong) rounded-[8px] px-2 lg:px-4 py-2 text-[13px] leading-[19px] font-semibold text-(--text-primary) hover:bg-[var(--brand-hover)] transition cursor-pointer flex items-center capitalize" onClick={() => setIsAddAssetModalOpen(true)}>
-                            <PlusIcon className="inline-block me-1" size={14} />
-                            Add Asset
-                        </button>
-                        <button className="bg-(--brand) rounded-[8px] px-2 lg:px-4 py-2 text-[13px] leading-[19px] font-semibold text-white hover:opacity-90 transition cursor-pointer flex items-center capitalize">
-                            <TrendingDown className="inline-block me-1" size={14} />
-                            Add exit
-                        </button>
+                </div>
+                <div className="mt-10 grid grid-cols-4 w-full gap-4">
+                    <div className="flex flex-col gap-2 bg-(--bg-surface) p-6 shadow=(--shadow-card) border border-(--border-subtle) rounded-[14px]">
+                        <p className="type-label text-[12px]/[16px] font-semibold text-(--text-secondary) capitalize">
+                            value
+                        </p>
+                        <h2 className="font-figure text-[32px] leading-[-0.32px] font-semibold tabular-nums text-[var(--text-primary)]">
+                            ${totalValue.toFixed(2)}
+                        </h2>
+                    </div>
+                    <div className="flex flex-col gap-2 bg-(--bg-surface) p-6 shadow=(--shadow-card) border border-(--border-subtle) rounded-[14px]">
+                        <p className="type-label text-[12px]/[16px] font-semibold text-(--text-secondary) capitalize">
+                            p&l
+                        </p>
+                        <h2 className="font-figure text-[32px] leading-[-0.32px] font-semibold tabular-nums text-[var(--positive)]">
+                            +${totalPnl.toFixed(2)}
+                        </h2>
+                    </div>
+                    <div className="flex flex-col gap-2 bg-(--bg-surface) p-6 shadow=(--shadow-card) border border-(--border-subtle) rounded-[14px]">
+                        <p className="type-label text-[12px]/[16px] font-semibold text-(--text-secondary) capitalize">
+                            ROI
+                        </p>
+                        <h2 className="font-figure text-[32px] leading-[-0.32px] font-semibold tabular-nums text-[var(--positive)]">
+                            <IoTriangleSharp className="inline-block me-1" size={18} /> {totalROI.toFixed(2)}%
+                        </h2>
+                    </div>
+                    <div className="flex flex-col gap-2 bg-(--bg-surface) p-6 shadow=(--shadow-card) border border-(--border-subtle) rounded-[14px]">
+                        <p className="type-label text-[12px]/[16px] font-semibold text-(--text-secondary) capitalize">
+                            No. of assets
+                        </p>
+                        <h2 className="font-figure text-[32px] leading-[-0.32px] font-semibold tabular-nums text-(--text-primary)">
+                            {assetNumber}
+                        </h2>
                     </div>
                 </div>
+                <AddAssetModal addAssetModalOpen={isAddAssetModalOpen} setAddAssetModalOpen={setIsAddAssetModalOpen} />
+                {
+                    loading ? (
+                        <div className="flex items-center justify-center w-full h-[400px]">
+                            <p className="text-(--text-secondary) text-[14px] leading-[20px] font-normal">
+                                Loading...
+                            </p>
+                        </div>
+                    ) : error ? (
+                        <div className="flex items-center justify-center w-full h-[400px]">
+                            <p className="text-(--text-secondary) text-[14px] leading-[20px] font-normal">Error: {error instanceof Error ? error.message : String(error)}</p>
+                        </div>
+                    ) : (
+                        <AssetTrackerTable  isAssetModalOpen={isAddAssetModalOpen} setIsAddAssetModalOpen={setIsAddAssetModalOpen} assets={assets} totalValue={totalValue} totalPnl={totalPnl} assetNumber={assetNumber} totalROI={totalROI} />
+                    )
+                }
             </div>
-            <div className="mt-10 grid grid-cols-4 w-full gap-4">
-                <div className="flex flex-col gap-2 bg-(--bg-surface) p-6 shadow=(--shadow-card) border border-(--border-subtle) rounded-[14px]">
-                    <p className="type-label text-[12px]/[16px] font-semibold text-(--text-secondary) capitalize">
-                        value
-                    </p>
-                    <h2 className="font-figure text-[32px] leading-[-0.32px] font-semibold tabular-nums text-[var(--text-primary)]">
-                        $12,345.67
-                    </h2>
-                </div>
-                <div className="flex flex-col gap-2 bg-(--bg-surface) p-6 shadow=(--shadow-card) border border-(--border-subtle) rounded-[14px]">
-                    <p className="type-label text-[12px]/[16px] font-semibold text-(--text-secondary) capitalize">
-                        p&l
-                    </p>
-                    <h2 className="font-figure text-[32px] leading-[-0.32px] font-semibold tabular-nums text-[var(--positive)]">
-                        +$1,234.56
-                    </h2>
-                </div>
-                <div className="flex flex-col gap-2 bg-(--bg-surface) p-6 shadow=(--shadow-card) border border-(--border-subtle) rounded-[14px]">
-                    <p className="type-label text-[12px]/[16px] font-semibold text-(--text-secondary) capitalize">
-                        ROI
-                    </p>
-                    <h2 className="font-figure text-[32px] leading-[-0.32px] font-semibold tabular-nums text-[var(--positive)]">
-                        <IoTriangleSharp className="inline-block me-1" size={18} /> 34.72%
-                    </h2>
-                </div>
-                <div className="flex flex-col gap-2 bg-(--bg-surface) p-6 shadow=(--shadow-card) border border-(--border-subtle) rounded-[14px]">
-                    <p className="type-label text-[12px]/[16px] font-semibold text-(--text-secondary) capitalize">
-                        No. of assets
-                    </p>
-                    <h2 className="font-figure text-[32px] leading-[-0.32px] font-semibold tabular-nums text-(--text-primary)">
-                        5
-                    </h2>
-                </div>
-            </div>
-            <AddAssetModal addAssetModalOpen={isAddAssetModalOpen} setAddAssetModalOpen={setIsAddAssetModalOpen} />
-            <div className="flex items-center justify-center mt-8 mb-5 w-fit gap-6">
-                {filters.map((filter) => (
-                    <button
-                        key={filter.value}
-                        className='flex items-center justify-center outline-none border-b-2 px-1 py-0.5 border-(--brand) bg-transparent cursor-pointer text-[13px] text-(--text-primary) capitalize ease-linear duration-150 font-semibold tracking-[-0.56px]'
-                    >
-                        {filter.name}
-                    </button>
-                ))}
-            </div>
-            <AssetTrackerTable isAssetModalOpen={isAddAssetModalOpen} setIsAddAssetModalOpen={setIsAddAssetModalOpen} />
         </div>
     )
 }
--- src/components/ui/AssetTrackerTable.tsx
+++ src/components/ui/AssetTrackerTable.tsx
@@ -1,53 +1,81 @@
-import React from 'react'
-import {useAssetData} from '@/hooks/useAssetData';
+import React, { act, useState } from 'react'
+import { AssetData } from '@/hooks/useAssetData';
 
 type Props = {
     setIsAddAssetModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
     isAssetModalOpen: boolean;
+    assets: AssetData[]; 
+    totalValue: number;
+    totalPnl: number;
+    assetNumber: number;
+    totalROI: number;
+    status?: "live" | "closed" | "deleted";
 }
 
 const AssetTrackerTable = (props: Props) => {
-    const { assets, loading, error } = useAssetData();
-    const liveAssets = assets.filter(asset => asset.status === 'live');
-    const closedAssets = assets.filter(asset => asset.status === 'closed');
-    const deletedAssets = assets.filter(asset => asset.status === 'deleted');
-    const totalValue = liveAssets.reduce((acc, asset) => acc + (asset.purchasePrice * asset.quantity), 0);
-    const totalPnl = liveAssets.reduce((acc, asset) => acc + (asset.purchasePrice * asset.quantity * 0.1), 0);
-    const assetNumber = liveAssets.length;
-    const totalROI = totalValue > 0 ? (totalPnl / totalValue) * 100 : 0;
+    const { assets, totalValue, totalPnl, assetNumber, totalROI, status } = props;
+    const filters = [
+        { name: 'live assets', value: 'live' },
+        { name: 'closed assets', value: 'closed' },
+        { name: 'deleted assets', value: 'deleted' },
+    ];
+    const [activeFilter, setActiveFilter] = useState('live');
+    const filteredAssets = assets.filter(asset => asset.status === activeFilter);
 
     return (
-        <div className="max-h-[600px] overflow-y-scroll  border border-(--border-subtle) bg-(--bg-surface) rounded-2xl shadow-(--shadow-card) pb-10">
-            <table className="table-auto w-full">
-                <thead className="sticky top-0  z-10">
-                    <tr className="bg-(--bg-surface-2) border-b border-(--border-subtle) hover:bg-(--bg-surface) ease-in duration-200 cursor-pointer">
-                        <th className='text-left text-[12px]/[16px] font-medium font-mono trackng-[1px] text-(--text-secondary) py-3 px-4'>Asset</th>
-                        <th className='text-right text-[12px]/[16px] font-medium font-mono trackng-[1px] text-(--text-secondary) py-3 px-4'>Live Qty</th>
-                        <th className='text-right text-[12px]/[16px] font-medium font-mono trackng-[1px] text-(--text-secondary) py-3 px-4'>Avg Cost</th>
-                        <th className='text-right text-[12px]/[16px] font-medium font-mono trackng-[1px] text-(--text-secondary) py-3 px-4'>Price</th>
-                        <th className='text-right text-[12px]/[16px] font-medium font-mono trackng-[1px] text-(--text-secondary) py-3 px-4'>Invested</th>
-                        <th className='text-right text-[12px]/[16px] font-medium font-mono trackng-[1px] text-(--text-secondary) py-3 px-4'>Value</th>
-                        <th className='text-right text-[12px]/[16px] font-medium font-mono trackng-[1px] text-(--text-secondary) py-3 px-4'>Pnl</th>
-                        <th className='text-right text-[12px]/[16px] font-medium font-mono trackng-[1px] text-(--text-secondary) py-3 px-4'>Updated</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        liveAssets.map((asset) => (
-                            <tr key={asset._id} className="border-b border-(--border-subtle) hover:bg-(--bg-surface) ease-in duration-200 cursor-pointer">
-                                <td className='text-left text-[13px]/[16px] font-medium font-mono trackng-[1px] text-(--text-primary) py-3 px-4'>{asset.symbol}</td>
-                                <td className='text-right text-[13px]/[16px] font-medium font-mono trackng-[1px] text-(--text-primary) py-3 px-4'>{asset.quantity}</td>
-                                <td className='text-right text-[13px]/[16px] font-medium font-mono trackng-[1px] text-(--text-primary) py-3 px-4'>${asset.purchasePrice.toFixed(2)}</td>
-                                <td className='text-right text-[13px]/[16px] font-medium font-mono trackng-[1px] text-(--text-primary) py-3 px-4'>${(asset.purchasePrice * 1.1).toFixed(2)}</td>
-                                <td className='text-right text-[13px]/[16px] font-medium font-mono trackng-[1px] text-(--text-primary) py-3 px-4'>${(asset.purchasePrice * asset.quantity).toFixed(2)}</td>
-                                <td className='text-right text-[13px]/[16px] font-medium font-mono trackng-[1px] text-(--text-primary) py-3 px-4'>${(asset.purchasePrice * asset.quantity * 1.1).toFixed(2)}</td>
-                                <td className='text-right text-[13px]/[16px] font-medium font-mono trackng-[1px] text-(--text-primary) py-3 px-4'>${(asset.purchasePrice * asset.quantity * 0.1).toFixed(2)}</td>
-                                <td className='text-right text-[13px]/[16px] font-medium font-mono trackng-[1px] text-(--text-primary) py-3 px-4'>{new Date(asset.updatedAt).toLocaleDateString()}</td>
+        <div className="">
+            <div className="flex items-center justify-center mt-8 mb-5 w-fit gap-6">
+                {filters.map((filter) => (
+                    <button
+                        key={filter.value}
+                        onClick={() => {
+                            setActiveFilter(filter.value)
+                        }}
+                        className={`flex items-center justify-center outline-none ${activeFilter === filter.value ? "border-b-2 border-(--brand)" : "border-b-2 border-transparent"}  px-1 py-0.5 bg-transparent cursor-pointer text-[13px] text-(--text-primary) capitalize ease-linear duration-150 font-semibold tracking-[-0.56px]`}
+                    >
+                        {filter.name}
+                    </button>
+                ))}
+            </div>
+            <div className="max-h-[600px] overflow-y-scroll  border border-(--border-subtle) bg-(--bg-surface) rounded-2xl shadow-(--shadow-card)">
+                <table className="table-auto w-full">
+                    <thead className="sticky top-0  z-10">
+                        <tr className="bg-(--bg-surface-2) border-b border-(--border-subtle) hover:bg-(--bg-surface) ease-in duration-200 cursor-pointer">
+                            <th className='text-left text-[12px]/[16px] font-medium font-mono trackng-[1px] text-(--text-secondary) py-3 px-4'>Asset</th>
+                            <th className='text-right text-[12px]/[16px] font-medium font-mono trackng-[1px] text-(--text-secondary) py-3 px-4'>Live Qty</th>
+                            <th className='text-right text-[12px]/[16px] font-medium font-mono trackng-[1px] text-(--text-secondary) py-3 px-4'>Avg Cost</th>
+                            <th className='text-right text-[12px]/[16px] font-medium font-mono trackng-[1px] text-(--text-secondary) py-3 px-4'>Price</th>
+                            <th className='text-right text-[12px]/[16px] font-medium font-mono trackng-[1px] text-(--text-secondary) py-3 px-4'>Invested</th>
+                            <th className='text-right text-[12px]/[16px] font-medium font-mono trackng-[1px] text-(--text-secondary) py-3 px-4'>Value</th>
+                            <th className='text-right text-[12px]/[16px] font-medium font-mono trackng-[1px] text-(--text-secondary) py-3 px-4'>Pnl</th>
+                            <th className='text-right text-[12px]/[16px] font-medium font-mono trackng-[1px] text-(--text-secondary) py-3 px-4'>Updated</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            filteredAssets.length > 0 ? (
+                                filteredAssets.map((asset) => (
+                                    <tr key={asset._id} className="border-b last-of-type:border-0 border-(--border-subtle) hover:bg-(--bg-surface) ease-in duration-200 cursor-pointer">
+                                        <td className='text-left text-[13px]/[16px] font-medium font-mono trackng-[1px] text-(--text-primary) py-3 px-4'>{asset.symbol}</td>
+                                        <td className='text-right text-[13px]/[16px] font-medium font-mono trackng-[1px] text-(--text-primary) py-3 px-4'>{asset.quantity}</td>
+                                        <td className='text-right text-[13px]/[16px] font-medium font-mono trackng-[1px] text-(--text-primary) py-3 px-4'>${asset.purchasePrice.toFixed(2)}</td>
+                                        <td className='text-right text-[13px]/[16px] font-medium font-mono trackng-[1px] text-(--text-primary) py-3 px-4'>${(asset.purchasePrice * 1.1).toFixed(2)}</td>
+                                        <td className='text-right text-[13px]/[16px] font-medium font-mono trackng-[1px] text-(--text-primary) py-3 px-4'>${(asset.purchasePrice * asset.quantity).toFixed(2)}</td>
+                                        <td className='text-right text-[13px]/[16px] font-medium font-mono trackng-[1px] text-(--text-primary) py-3 px-4'>${(asset.purchasePrice * asset.quantity * 1.1).toFixed(2)}</td>
+                                        <td className='text-right text-[13px]/[16px] font-medium font-mono trackng-[1px] text-(--text-primary) py-3 px-4'>${(asset.purchasePrice * asset.quantity * 0.1).toFixed(2)}</td>
+                                        <td className='text-right text-[13px]/[16px] font-medium font-mono trackng-[1px] text-(--text-primary) py-3 px-4'>{new Date(asset.updatedAt).toLocaleDateString()}</td>
+                                    </tr>
+                                ))
+                            ) :
+                            <tr>
+                                <td colSpan={8} className="text-center py-8 text-[13px] font-normal text-(--text-secondary)">
+                                    No active assets found matching <span className="font-semibold capitalize text-(--text-primary">"{activeFilter}"</span>  assets
+                                </td>
                             </tr>
-                        ))
-                    }
-                </tbody>
-            </table>
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     )
 }
